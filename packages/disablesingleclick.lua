2ecad0f1bfd416f5a2d6783bc079713c27873bc8
sclark39 <sclark@freolic.com>
2014-05-17 12:29:21 -0700

    Disabling click-to-rename, but making it so clicking to the left of a folder entry toggles it

--- packages/disablesingleclick.lua
+++ packages/disablesingleclick.lua
@@ -8,15 +8,34 @@ local IMG_DIRECTORY = 0
 	version = 0.1, 
 	onFiletreeLDown = 
 		function(self, tree, event, item_id ) 
-			--local mask = wx.wxTREE_HITTEST_ONITEMINDENT
-			--	+ wx.wxTREE_HITTEST_ONITEMICON + wx.wxTREE_HITTEST_ONITEMRIGHT
-			--local item_id, flags = tree:HitTest(event:GetPosition())
-			if item_id then -- and bit.band(flags, mask) > 0 then
-				tree:SelectItem(item_id)
-				tree:SetFocus()
-			else  
-			--	event:Skip()
+			local selectmask =
+				wx.wxTREE_HITTEST_ONITEMRIGHT +
+				wx.wxTREE_HITTEST_ONITEMLABEL +
+				wx.wxTREE_HITTEST_ONITEMINDENT
+			
+			local expanddirmask = 
+				wx.wxTREE_HITTEST_ONITEMINDENT						
+				
+			local mask = tree:GetItemImage(item_id) == IMG_DIRECTORY and dirmask or filemask		
+			local item_id, flags = tree:HitTest(event:GetPosition())
+			
+			if item_id then				
+				if tree:GetItemImage(item_id) == IMG_DIRECTORY then
+					if bit.band(flags, expanddirmask) > 0 then
+						tree:Toggle(item_id)
+						tree:SelectItem(item_id)
+						return false
+					end
+				end
+														
+				if bit.band(flags, selectmask) > 0 then
+					tree:SelectItem(item_id)
+					tree:SetFocus()
+					return false
+				end
 			end
+			
+			event:Skip()
 			return false
 		end,
 	onFiletreeLDClick = 
